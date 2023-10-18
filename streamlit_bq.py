4788b4517b2a5184c81504b33db220f953814f03
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-18 18:51:16 +0530

    Update streamlit_bq.py

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -9,15 +9,16 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 client = bigquery.Client(credentials=credentials)
+st.write(type(client))
+st.write(client)
 st.subheader("Enter patient's details below")
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 # @st.cache_data(ttl=600)
 def run_query(query):
-    st.write("Some wise words from Shakespeare:")
+    # st.write("Some wise words from Shakespeare:")
     query_job = client.query(query)
     st.write(client)
-    st.write("Some wise words from Shakespeare:")
     
     rows_raw = query_job.result()
     # Convert to list of dicts. Required for st.cache_data to hash the return value.
