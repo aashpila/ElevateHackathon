fc183ef69f1cc1f27208fc9c8638d525d4b8a9e0
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-18 18:23:42 +0530

    Update streamlit_bq.py

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -10,19 +10,19 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = bigquery.Client(credentials=credentials)
 st.subheader("Enter patient's details below")
-# # Perform query.
-# # Uses st.cache_data to only rerun when the query changes or after 10 min.
-# @st.cache_data(ttl=600)
-# def run_query(query):
-#     query_job = client.query(query)
-#     rows_raw = query_job.result()
-#     # Convert to list of dicts. Required for st.cache_data to hash the return value.
-#     rows = [dict(row) for row in rows_raw]
-#     return rows
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-# rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+rows = run_query("SELECT word FROM `medadsquad.patient_reg_db.patient_info` LIMIT 10")
 
-# # Print results.
-# st.write("Some wise words from Shakespeare:")
-# for row in rows:
-#     st.write(" " + row['word'])
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write(" " + row['word'])
