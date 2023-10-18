4a1b227f442f031b5a006bac82025cfc1b68a86d
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-18 17:47:21 +0530

    Update streamlit_bq.py

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -4,25 +4,25 @@ from google.cloud import bigquery
 st.title("Patient Registration")
 st.subheader("Enter patient's details below")
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
+# # Create API client.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"]
+# )
+# client = bigquery.Client(credentials=credentials)
 
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
+# # Perform query.
+# # Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+# def run_query(query):
+#     query_job = client.query(query)
+#     rows_raw = query_job.result()
+#     # Convert to list of dicts. Required for st.cache_data to hash the return value.
+#     rows = [dict(row) for row in rows_raw]
+#     return rows
 
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+# rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
-# Print results.
-st.write("Some wise words from Shakespeare:")
-for row in rows:
-    st.write(" " + row['word'])
+# # Print results.
+# st.write("Some wise words from Shakespeare:")
+# for row in rows:
+#     st.write(" " + row['word'])
