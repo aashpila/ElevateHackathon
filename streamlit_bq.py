3bd49fa8be74c69e59ad01c6b282cfd7ac85a60b
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-18 17:44:03 +0530

    Update streamlit_bq.py

--- streamlit_bq.py
+++ streamlit_bq.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
+st.title("Patient Registration")
+st.subheader("Enter patient's details below")
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
