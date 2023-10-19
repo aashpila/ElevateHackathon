502db186920f4d624f3227a0f18a914279582131
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-19 13:03:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.form("form1", clear_on_submit=True):
   drugstr = st.slider("Enter medication strength", min_value = 1, max_value = 1000)
   druguom = st.multiselect("Enter medication UOM",druguomlist)
   contact = st.number_input('Enter contact number', min_value=1000000000, max_value=9999999999)
-  ml_nonadhere_score = st.number_input("Enter Patient's Non Adherence Score", min_value=0.0, max_value=1.0,format="%.2f")
+  ml_nonadhere_score = st.number_input("Enter Patients Non Adherence Score", min_value=0.0, max_value=1.0,format="%.2f")
   doctor = st.text_input("Enter prescriber full name")
   medication_time = st.time_input('Enter medication time', value=None)
   frequency = st.multiselect("Enter medication frequency",freqlist)
