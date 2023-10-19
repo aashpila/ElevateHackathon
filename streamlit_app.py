0f1f83f026786b9695cce86f17b869e47954bf85
aashpila <109336293+aashpila@users.noreply.github.com>
2023-10-19 13:02:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,19 +15,19 @@ def call_success_func():
 
 #CREATING OUR FORM FIELDS
 with st.form("form1", clear_on_submit=True):
+  seq_id=st.number_input('Enter Patient ID', min_value=0, max_value=1000)
   patient_name = st.text_input("Enter patient full name")
   medication = st.text_input("Enter medication name")
   drugstr = st.slider("Enter medication strength", min_value = 1, max_value = 1000)
   druguom = st.multiselect("Enter medication UOM",druguomlist)
   contact = st.number_input('Enter contact number', min_value=1000000000, max_value=9999999999)
-  ml_nonadhere_score = st.number_input('Enter contact number', min_value=0.0, max_value=1.0,format="%.2f")
+  ml_nonadhere_score = st.number_input('Enter Patient's Non Adherence Score', min_value=0.0, max_value=1.0,format="%.2f")
   doctor = st.text_input("Enter prescriber full name")
   medication_time = st.time_input('Enter medication time', value=None)
   frequency = st.multiselect("Enter medication frequency",freqlist)
   interests = st.text_area("Enter patient's interests")
   location = st.text_input("Enter patient's city")
-  streak_value=st.number_input('Enter contact number', min_value=0, max_value=10)
-  seq_id=st.number_input('Enter contact number', min_value=0, max_value=1000)
+  streak_value=st.number_input('Enter patient's streak value', min_value=0, max_value=10)
   submit = st.form_submit_button("Submit Details", on_click=call_success_func)
   drug_str_uom=str(drugstr)+" "+str(druguom)
   insert_query = f""" INSERT INTO `{table_name}` (patient_name, medication , dosage , doctor ,interests ,
